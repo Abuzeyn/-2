cb9ba00aa9b7ae55ac4a92aa810c5649063d5ef5
User <User@DESKTOP-ILM46JN>
2023-04-09 20:24:57 +0400

    Add project files.

---
+++ Черновик2/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Черновик2
+{
+    public static class Class1
+    {
+        public static double ReadFromConsoleSum()
+        {
+            Console.WriteLine("Введите сумму вклада");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            return sum;
+        }
+
+        public static double ReadFromConsoleDepositPeriod()
+        {
+            Console.WriteLine("Срок хранения вклада в годах");
+            double depositPeriod = Convert.ToDouble(Console.ReadLine());
+            return depositPeriod;
+
+        }
+
+        public static double ReadFromConsolePerсentYear()
+        {
+            Console.WriteLine("Процент начисляемый за год");
+            double percentYear = Convert.ToDouble(Console.ReadLine());
+            return percentYear;
+        }
+
+        public static void CalculatorEndAnswer(double sum, double depositPeriod, double percentYear)
+        {
+            double a = (percentYear + 100) / 100;
+            for (double i = 1; i <= depositPeriod; i++)
+            {
+                sum = sum * a;
+
+            }
+            Console.WriteLine(sum);
+
+
+        }
+
+
+
+    }
+}
---
+++ Черновик2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Черновик2;
+double x = Class1.ReadFromConsoleSum();
+double y = Class1.ReadFromConsoleDepositPeriod();
+double z = Class1.ReadFromConsolePerсentYear();
+Class1.CalculatorEndAnswer(x, y, z);
+
+
